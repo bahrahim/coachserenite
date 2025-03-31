d5a2d6350ee7d79ff946a18f60a93a9e80fc7b20
Rahim BAH <abdoulrahim.bahh@gmail.com>
2025-04-01 01:11:51 +0200

    mis en place de git pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/coachserenite/', // Ajoute le nom EXACT de ton repo ici
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
